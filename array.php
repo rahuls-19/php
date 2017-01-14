008f19155d914fc98e1e68624fb066e5482b8aa0
Rahul kumar <lit2015023@iiita.ac.in>
2017-01-14 06:30:36 -0800

    array

---
+++ array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Array Part - 1</title>
+</head>
+<body>
+	<?php
+		$arr = [10,15,23.04,"rahul","kumar"];
+		echo ($arr[2])."</br>";
+		print_r($arr);
+		$myarr = [21,34,54,[13,32,12]];
+		echo $myarr[3][2]."</br>";
+		print_r($myarr);
+		?>
+</body>
+</html>
