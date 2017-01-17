e11a5f87a9a793f10aa82e6ac2f90f949f666bfb
Rahul kumar <lit2015023@iiita.ac.in>
2017-01-18 02:15:24 +0530

    associate array

---
+++ associated_array.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Associated Array</title>
+</head>
+<body>
+<?php
+    $arr =["name" => "Rahul Kumar", "College name" => "IIITA"];
+    echo $arr["name"]. "<br/>";
+    echo $arr["College name"];
+    ?>
+</body>
+</html>
