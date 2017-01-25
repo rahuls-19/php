388f5f6b9155b06b8fc2b98633ab259e033a74ad
Rahul kumar <lit2015023@iiita.ac.in>
2017-01-25 23:57:27 +0530

    word censering without case sensitivity

--- word_censoring.php
+++ word_censoring.php
@@ -2,7 +2,7 @@
 $find = array("akash","ashmit","abhishek");
 $replace = array("alex","starc","virat");
 if(isset($_POST['user_input']) && !empty($_POST['user_input'])){
-  echo $_POST['user_input'];
+//  echo $_POST['user_input'];
  $user_input = $_POST['user_input'];
  $user_input_new = str_ireplace($find,$replace,$user_input);
  echo $user_input_new;
