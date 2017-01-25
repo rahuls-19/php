8896f721bad6e1b35889bbea42d23e99680175fe
Rahul kumar <lit2015023@iiita.ac.in>
2017-01-25 23:41:39 +0530

    word censoring part-1

---
+++ word_censoring.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST['user_input']) && !empty($_POST['user_input']))
+  echo $user_input = $_POST['user_input'];
+
+
+
+
+ ?>
+ <hr>
+ <form action = "word_censoring.php" method = "POST" >
+    <textarea name = "user_input" rows = "6" cols = "30" ></textarea><br><br>
+    <input type = "submit" value = "submit">
+  </form>
