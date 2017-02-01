f153ee2c3f358c7083aeecf5585a1f2d816aea17
Rahul kumar <lit2015023@iiita.ac.in>
2017-02-01 10:55:05 +0530

    md5encription

--- md5encription.php
+++ md5encription.php
@@ -1,6 +1,22 @@
 <?php
-$string = 'Yahin hoon main ,yahin hoon main kahin naa gaya tujhe chodkr main';
-$string1 = 'password';
- echo md5($string1);
+if(isset($_POST['user_password']) && !empty($_POST['user_password'])){
+echo $user_password = md5($_POST['user_password']);
+$filename = 'md5.txt';
+$handle = fopen($filename,'r');
+echo $filepassword = fread($handle,filesize($filename));
+  if($filepassword != $user_password){
+    echo 'Password is not  correct';
+  }
+  else{
+    echo 'Password is correct';
+  }
+}
+else{
+  echo 'Please enter the password';
+}
 
  ?>
+ <form action = 'md5encription.php' method = 'POST'>
+   Password:<input type = 'text' name = 'user_password'><br><br>
+   <input type = 'submit' value = 'submit'><br>
+ </form>
